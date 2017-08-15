efef2614c7d58bd6a86fbbda939d571e2ac1a76e
Maati Lasly <maati.lasly@adviqo.com>
2017-08-15 16:36:18 +0200

    test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const API_KEY='2a7364489dc79cfca1bdcbed58b3c722';
+//const API_KEY='a82d872c6ac02a9b041ec8ca35d8fd65';
 
-const ROOT_URL=`http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+//const ROOT_URL=`http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 const FETCH_WEATHER='FETCH_WEATHER';
 
 export function fetchWeather(city){
